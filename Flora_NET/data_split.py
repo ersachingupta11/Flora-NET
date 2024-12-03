57847a059be9d715abea5ec43b7eb5ed97c46681
ersachingupta11 <115605793+ersachingupta11@users.noreply.github.com>
2024-12-03 07:43:47 +0530

    Update data_split.py

--- Flora_NET/data_split.py
+++ Flora_NET/data_split.py
@@ -40,6 +40,31 @@ dataset = dataset_path.ImageFolder(data_dir, transform=transform)
 #Split the dataset into the Training (80%), Validation (10%), and Testing (10%)
 splitfolders.ratio(dataset_path, output=output_path, seed=42, ratio=(.8, .1, .1))
 #Create the Folders for Training, Validation and Testing
-train_dataset = "/content/drive/My Drive/split_flower_datasets/train"
-val_dataset = "/content/drive/My Drive/split_flower_datasets/val"
-test_dataset = "/content/drive/My Drive/split_flower_datasets/test"
+train_dataset_path = "/content/drive/My Drive/split_flower_datasets/train"
+val_dataset_path = "/content/drive/My Drive/split_flower_datasets/val"
+test_dataset_path = "/content/drive/My Drive/split_flower_datasets/test"
+
+import sys
+new_batch = int(sys.argv[3])
+
+# Batch size
+batch_size = new_batch  # Set your batch size here
+
+# Create DataLoaders for training, validation, and testing
+train_dataset = DataLoader(
+    datasets.ImageFolder(train_dataset_path, transform=transform),
+    batch_size=batch_size,
+    shuffle=True  # Shuffle training data
+)
+
+val_dataset = DataLoader(
+    datasets.ImageFolder(val_dataset_path, transform=transform),
+    batch_size=batch_size,
+    shuffle=False  # No need to shuffle validation data
+)
+
+test_dataset = DataLoader(
+    datasets.ImageFolder(test_dataset_path, transform=transform),
+    batch_size=batch_size,
+    shuffle=False  # No need to shuffle testing data
+)
